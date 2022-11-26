85943fb4662aab2cc4ce987a7c4572cc1863a5b8
zhaojun <zhaojun@chuango.com>
2022-11-26 10:51:33 +0800

    添加测试代码

--- main.go
+++ main.go
@@ -1,10 +1,14 @@
 package main
 
 import (
+	"context"
+	proto "easy-go-iot/user-web/proto"
 	"fmt"
+	"google.golang.org/grpc"
 	"os"
 	"os/signal"
 	"syscall"
+	"time"
 
 	"github.com/gin-gonic/gin/binding"
 	ut "github.com/go-playground/universal-translator"
@@ -55,6 +59,42 @@ func main() {
 	if err := Router.Run(fmt.Sprintf(":%d", global.ServerConfig.Port)); err != nil {
 		zap.S().Panic("启动失败:", err.Error())
 	}
+
+	go func() {
+		time.Sleep(10 * time.Second)
+		userConn, err := grpc.Dial(
+			fmt.Sprintf("%s:%d", "userrpc-service", 50001),
+			grpc.WithInsecure())
+		if err != nil {
+			zap.S().Fatal("[InitSrvConn] 连接 【用户服务失败】")
+		}
+		userClient := proto.NewUserClient(userConn)
+		rsp, err := userClient.GetUserById(context.Background(), &proto.IdRequest{
+			Id: int32(1),
+		})
+		if err != nil {
+			zap.S().Error(err)
+		}
+		zap.S().Info(rsp.Mobile, rsp.NickName, rsp.Password)
+	}()
+
+	go func() {
+		time.Sleep(20 * time.Second)
+		userConn, err := grpc.Dial(
+			fmt.Sprintf("%s:%d", global.ServerConfig.UserSrvInfo.Host, global.ServerConfig.UserSrvInfo.Port),
+			grpc.WithInsecure())
+		if err != nil {
+			zap.S().Fatal("[InitSrvConn] 连接 【用户服务失败】")
+		}
+		userClient := proto.NewUserClient(userConn)
+		rsp, err := userClient.GetUserById(context.Background(), &proto.IdRequest{
+			Id: int32(1),
+		})
+		if err != nil {
+			zap.S().Error(err)
+		}
+		zap.S().Info(rsp.Mobile, rsp.NickName, rsp.Password)
+	}()
 
 	//接收终止信号
 	quit := make(chan os.Signal)
