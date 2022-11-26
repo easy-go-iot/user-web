225222c31a4539bead0326a6b71cc98aac8011f2
zhaojun <zhaojun@chuango.com>
2022-11-26 11:29:21 +0800

    替换手机号正则表达式

--- validator/validators.go
+++ validator/validators.go
@@ -9,7 +9,7 @@ import (
 func ValidateMobile(fl validator.FieldLevel) bool {
 	mobile := fl.Field().String()
 	//使用正则表达式判断是否合法
-	ok, _ := regexp.MatchString(`^1([38][0-9]|14[579]|5[^4]|16[6]|7[1-35-8]|9[189])\d{8}$`, mobile)
+	ok, _ := regexp.MatchString(`^(13[0-9]|14[01456879]|15[0-35-9]|16[2567]|17[0-8]|18[0-9]|19[0-35-9])\d{8}$`, mobile)
 	if !ok {
 		return false
 	}
